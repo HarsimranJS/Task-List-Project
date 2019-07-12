ccad039b46026e121e35c9e82093945a2dad409a
HarsimranJS <40564724+HarsimranJS@users.noreply.github.com>
2019-07-12 15:26:54 +0530

    Added the functionality to use local storage to keep the data persistant

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function loadEventListeners(){
     taskList.addEventListener('click',closeTask);
     clearBtn.addEventListener('click',clearAllTasks);
     filter.addEventListener('keyup',filterTasks);
+    document.addEventListener('DOMContentLoaded',fetchTasksFromStorage);
 }
 
 //Function to add task
@@ -37,6 +38,8 @@ function addTask(e){
     li.appendChild(x);
     e.preventDefault();
 
+    addToLocalStorage(taskInput.value);
+
     //clear input
     taskInput.value="";
 }
@@ -47,6 +50,7 @@ function closeTask(e){
         if(confirm('Are you sure?')){
             e.target.parentNode.parentNode.remove();
         }
+        removeTaskFromLocalStorage(e.target.parentElement.parentElement.textContent);
 
     }
         
@@ -57,6 +61,7 @@ function clearAllTasks(e){
         collection.forEach(function(e){
              e.remove();
         })
+        clearAllTasksFromLocalStorage();
 }
 
 function filterTasks(e){
@@ -72,4 +77,64 @@ function filterTasks(e){
             current.style.display='none';
         }
     })
+}
+
+function addToLocalStorage(value){
+    let tasks;
+    if(localStorage.getItem('task')===null)
+    {
+        tasks=[];
+    }
+    else
+    {
+        tasks=JSON.parse(localStorage.getItem('task'));
+    }
+    tasks.push(value);
+    localStorage.setItem('task',JSON.stringify(tasks));
+
+}
+
+function fetchTasksFromStorage()
+{
+    let tasks;
+    if(localStorage.getItem('task')===null)
+    {
+        tasks=[];
+    }
+    else
+    {
+        tasks=JSON.parse(localStorage.getItem('task'));
+    }
+    tasks.forEach(function(task){
+        const li = document.createElement('li');
+        li.className='collection-item';
+        li.innerText=task;
+        taskList.appendChild(li);
+        const x=document.createElement('a');
+        x.className="delete-item secondary-content"
+        x.setAttribute('href',"#");
+        const y = document.createElement('i');
+        y.className='fa fa-remove';
+        x.appendChild(y);
+        li.appendChild(x);
+
+    })   
+}
+function removeTaskFromLocalStorage(task)
+{
+    let tasks=JSON.parse(localStorage.getItem('task'));
+    tasks.forEach(function(val,index){
+        if(val===task)
+        {
+            tasks.splice(index,1);
+        }
+    })
+    localStorage.setItem('task',JSON.stringify(tasks));
+}
+
+function clearAllTasksFromLocalStorage()
+{
+    let tasks=JSON.parse(localStorage.getItem('task'));
+    tasks=[];
+    localStorage.setItem('task',JSON.stringify(tasks));
 }
